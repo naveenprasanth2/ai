948a16ca4809d2075e11e921dde5e846d9f22ae7
Naveen Prasanth Ramasamy <naveenprasanth12@gmail.com>
2025-09-24 08:02:11 +0530

    singleton implementation practise

--- basics/intendly/wrap_learn.py
+++ basics/intendly/wrap_learn.py
@@ -2,6 +2,9 @@ from functools import wraps
 from typing import Callable, Any
 import time
 
+def get_time(func : Callable) -> Callable:
+    '''Gets the time of the given function'''
+    return func
 
 def expensive_function() -> None:
     '''expensive_function() docstring'''
