c64825d86367151ccb5a18712f05b951507f0dd0
Naveen Prasanth Ramasamy <naveenprasanth12@gmail.com>
2025-10-02 07:53:41 +0100

    file handling code with singleton code

---
+++ basics/intendly/singleton_3.py
@@ -0,0 +1,22 @@
+from typing import Any, Optional
+
+class Singleton:
+    _instance : Optional["Singleton"] = None
+
+    def __new__(cls, *args : Any, **kwargs : Any) -> "Singleton":
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+        return cls._instance
+
+    def __init__(self, value : int = 0) -> None:
+        if not hasattr(self, "__is_initialized"):
+            self.value = value
+            self._is_initialized = True
+
+if __name__ == '__main__':
+    s1 : Singleton = Singleton(30)
+    s2 : Singleton = Singleton(40)
+
+
+    print(s1.value)
+    print(s2.value)
--- basics/intendly/singleton_dec1.py
+++ basics/intendly/singleton_dec1.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict, TypeVar, Type, Callable, cast
+from typing import Any, Dict, TypeVar, Type, cast
 
 T = TypeVar('T')  # Generic type variable
 
